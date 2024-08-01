9b65d8eeeaafe85da0d240e2eae1c68699cc5411
SardarkhanStella <skhan@stellatechnology.com>
2024-08-01 14:31:25 +0500

    Add Api's for get and post mapping. and also add controller.

---
+++ src/main/java/com/stella/restApiApp/controller/AppEntryController.java
@@ -0,0 +1,27 @@
+package com.stella.restApiApp.controller;
+
+import com.stella.restApiApp.entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class AppEntryController {
+
+    private Map<Long,JournalEntity> listOfEntries = new HashMap<>();
+
+    @GetMapping("/getAll")
+    public List<JournalEntity> getAll(){
+        return new ArrayList<>(listOfEntries.values());
+    }
+
+    @PostMapping("/add")
+    public void createEntry(@RequestBody JournalEntity entry){
+        listOfEntries.put(entry.getId(),entry);
+    }
+
+}
